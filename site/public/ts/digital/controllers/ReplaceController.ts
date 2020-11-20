5c1378ff5724d8a63b4778fdc193084b02a5bac6
Henry Qi <henryqi301@gmail.com>
2020-11-20 17:58:10 -0500

    Filtered the JSON file

--- site/public/ts/digital/controllers/ReplaceController.ts
+++ site/public/ts/digital/controllers/ReplaceController.ts
@@ -6,9 +6,18 @@ import { OutputPort } from "digital/models/ports/OutputPort";
 import {DesignerController} from "site/shared/controllers/DesignerController";
 import Data from "site/data/digitalnavconfig.json";
 import {Create} from "serialeazy";
+import { data } from "jquery";
 export class ReplaceController extends DesignerController {
     public show(objs: IOObject[]): void{
 
+    }
+    public getID(item): string {
+        if(item.id){
+            return item.id;
+        }
+        else{
+            return;
+        }
     }
     //This returns an array that contains all the compatible components that matches in # of  I/O ports
     //from the selected array. We can use this array to display a menu that will allow that user to select
@@ -21,9 +30,14 @@ export class ReplaceController extends DesignerController {
         const ports = GetAllPorts(selection); //selection[0].getPorts(;
         const inputs = ports.filter(o => o instanceof InputPort);
         const outputs = ports.filter(o => o instanceof OutputPort);
-        //Get the Data and then make it into Components
-        const componentID = Data.sections;
-        const ComponentList = Array.from(componentID, x => Create<Component>(x.id) );
+        //Get the Data and then filter it so we can get the component IDs
+        const DataSection = Data.sections; 
+        //Within the sections, we need the items
+        const componentItems = Array.from(DataSection, x => x.items);
+        //Within the items, we need its ID
+        const componentID = Array.from(componentItems, x => this.getID(x));
+        //Using the ID, we create an array of components
+        const ComponentList = Array.from(componentID, x => Create<Component>(x));
         //From the ComponentList, find all the components that matches in the amount of input Ports and
         //outputPorts
         const CompatibleComponents = ComponentList.filter(o => (o.getPorts().filter(x => x instanceof InputPort).length == inputs.length
