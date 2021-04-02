b96e1a3d2775844c7b5a46f70ba99be884226576
= <=>
2021-04-02 17:28:45 -0400

    Fix issue with debug renderer

--- src/app/core/rendering/DebugRenderer.ts
+++ src/app/core/rendering/DebugRenderer.ts
@@ -39,7 +39,7 @@ export const DebugRenderer = (() => {
 
             if (DEBUG_PRESSABLE_BOUNDS) {
                 const pressables = objects.filter((c) => isPressable(c)) as Pressable[];
-                const cullboxes = pressables.map((p) => p.getPressableBoxes()).flatMap((p, i) => p[i]);
+                const cullboxes = pressables.flatMap((p) => p.getPressableBoxes());
                 renderer.save();
                 for (const cullBox of cullboxes) {
                     renderer.transform(camera, cullBox);
